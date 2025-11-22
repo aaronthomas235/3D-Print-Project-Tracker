34be05b36df52a48490ba1b46c554c44007ffa72
Aaron <aaron.tompkins@outlook.com>
2025-11-22 21:11:02 +0000

    Removed WPF References
    Removed any references to WPF and remove Interfaces, ViewModels and Services folders that were outside of 'Core'.

--- Interfaces/IExpanderItemHost.cs
+++
@@ -1,9 +0,0 @@
-﻿using _3DPrintProjectTracker.ViewModels;
-
-namespace _3DPrintProjectTracker.Interfaces
-{
-    public interface IExpanderItemHost
-    {
-        void OnExpanderItemSelected(ExpanderItemViewModel item);
-    }
-}
